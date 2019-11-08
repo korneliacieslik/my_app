b9e270f7bb5342f26e970e39b4c24d93011e24f2
korneliacieslik <korneliacieslik08@gmail.com>
2019-11-08 13:09:50 +0100

    Add Customer model with name and email attributes with validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+	# validate :name, :presence => true 
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true, 
+			  # :format {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+			  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
