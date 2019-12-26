681d3cb0d189b8b2215d7808dd0d0557661cfa56
korneliacieslik <korneliacieslik08@gmail.com>
2019-12-26 17:18:06 +0100

    Fix db failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :customers
   root 'static_pages#home'
   get 'static_pages/home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_081810) do
+ActiveRecord::Schema.define(version: 2019_12_26_161404) do
 
   create_table "customers", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
   end
 
 end
