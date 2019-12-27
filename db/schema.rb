2f6ce09786254644b79650c56b99dcfdaae9ea32
korneliacieslik <korneliacieslik08@gmail.com>
2019-12-27 14:57:02 +0100

    Add Order db model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_161404) do
+ActiveRecord::Schema.define(version: 2019_12_27_135621) do
 
   create_table "customers", force: :cascade do |t|
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
