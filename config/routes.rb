1a2c7b9f4236d8bd8626e3a4e01b27a10018164a
korneliacieslik <korneliacieslik08@gmail.com>
2020-01-14 23:06:38 +0100

    Add basic Opinion model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'opinion/index'
+  get 'opinion/new'
+  get 'opinion/create'
+  get 'opinion/destroy'
+  get 'opinion/edit'
   resources :orders
   resources :products
   resources :customers
+  resources :opinions
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/opinions', to: 'static_pages#opinions'
