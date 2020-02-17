421f2363876b9119b96ee5155a3c986771f2d596
Kornelia <korneliacieslik08@gmail.com>
2020-02-17 12:15:59 +0100

    Delete redundant space.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,15 +1,10 @@
 class Customer < ApplicationRecord
 	has_many :products, through: :order
 	has_many :orders
-	validates :name, presence: true,
-					 length: {minimum: 2}
+	validates :name, presence: true, length: {minimum: 2}
 	validates :email, presence: true 
 
-	#Rails method
-	#If both below are commented new customer can be signed up, else it doesn't work. ??
-	
+	#If both below are commented new customer can be signed up, else it doesn't work. ??	
 	#has_secure_password 
 	#validates :password , presence: true, length: {minimum: 6}  
-
-
 end
