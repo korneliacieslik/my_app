fd150ec956f5377fb390dbf5361150c9bfbe63cc
Kornelia <korneliacieslik08@gmail.com>
2020-02-18 15:11:30 +0100

    Fix update and destroy actions

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -37,19 +37,34 @@ class OpinionsController < ApplicationController
   end
 
   def destroy
+
+    @opinion = Opinion.find(params[:id])
+
+    @opinion.destroy
+
+    respond_to do |format|
+
+      format.html { redirect_to opinions_url, notice: 'Opinion was successfully destroyed.' }
+      
+      format.json { head :no_content }
+
+    end
   
   end
 
   def edit
-    @opinion =  Opinion.new
+
+    @opinion = Opinion.find(params[:id])
     
   end
 
   def update
 
+    @opinion = Opinion.find(params[:id])
+
     respond_to do |format|
 
-      if @opinion.edit
+      if @opinion.update(opinion_params)
 
         format.html  {redirect_to @opinion, notice: "Opinion was sucsessfully edit!"}
         
@@ -74,6 +89,12 @@ class OpinionsController < ApplicationController
   end 
 
   private
+
+    def set_opinion
+
+      @opinion = Opinion.find(params[:id])
+
+    end 
 
     def opinion_params
       
