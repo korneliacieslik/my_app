f6400e83ea49c2c6326746a0c68d9f7dbe447046
Kornelia <korneliacieslik08@gmail.com>
2020-02-27 18:51:15 +0100

    Add name attribute to User & upload views for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_102758) do
+ActiveRecord::Schema.define(version: 2020_02_27_174238) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_102758) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
