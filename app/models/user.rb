cc1713cff1bb4f4d8d0a47e38cd99edb3bef0106
Kornelia <korneliacieslik08@gmail.com>
2020-03-20 16:32:42 +0100

    Configure with omniauth-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable,
+         :omniauthable, omniauth_providers: %i[facebook]
+
 
   has_many :products, through: :order
   has_many :orders
   has_one  :opinion
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name 
+ 
+      user.skip_confirmation!
+    end
+  end
+    
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 
 end
