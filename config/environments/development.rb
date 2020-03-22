44260cafca3bf01ed9cd43321cf0f2d1f2e1a186
Kornelia <korneliacieslik08@gmail.com>
2020-03-22 22:24:23 +0100

    Add confirmation with email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -60,9 +57,22 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # set up the default URL options for the Devise mailer in each environment
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
 
+    config.action_mailer.smtp_settings = {
+    user_name:      "myapp.healthybowl@gmail.com",
+    password:       "myapp.hb!123",
+    domain:        "gmail.com",
+    address:       "smtp.gmail.com",
+    port:          "587",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
+  config.action_mailer.perform_deliveries = true
 
 end
