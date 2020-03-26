59e63b2df88cb61213d79531a1a2c942b87e1a71
Kornelia <korneliacieslik08@gmail.com>
2020-03-26 10:52:13 +0100

    Save sendmail_username & sendmail_password in .bashrc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
     config.action_mailer.smtp_settings = {
-    user_name:      "myapp.healthybowl@gmail.com",
-    password:       "myapp.hb!123",
+    user_name:      ENV["SENDMAIL_USERNAME"],
+    password:       ENV["SENDMAIL_PASSWORD"],
     domain:        "gmail.com",
     address:       "smtp.gmail.com",
     port:          "587",
