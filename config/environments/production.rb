e51cac5e28d90baf72f86a949d10b46e559fe837
Kornelia <korneliacieslik08@gmail.com>
2020-03-30 14:28:02 +0200

    Add delivery method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,5 +124,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = true
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 
 end
