7bceae397ae1d4d6d539d20e15dff7a0d70120f0
Kornelia <korneliacieslik08@gmail.com>
2020-03-31 22:57:46 +0200

    Add products photos with Active Storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,15 @@ product_list = [
 
 product_list.each do |name, description, price|  
   Product.create(name: name, description: description, price: price)
-end
+end 
+product_1 = Product.find_by name: 'Wegan bowl'
+product_1.photo.attach(io: File.open('app/assets/images/home_img_1.jpg'), filename: 'home_img_1.jpg', content_type: 'image/jpg')
+
+product_2 = Product.find_by name: 'Standard bowl'
+product_2.photo.attach(io: File.open('app/assets/images/home_img_2.jpg'), filename: 'home_img_2.jpg', content_type: 'image/jpg')
+
+product_3 = Product.find_by name: 'Active bowl'
+product_3.photo.attach(io: File.open('app/assets/images/home_img_3.jpg'), filename: 'home_img_3.jpg', content_type: 'image/jpg')
+
+product_4 = Product.find_by name: 'Fit bowl'
+product_4.photo.attach(io: File.open('app/assets/images/home_img_4.jpg'), filename: 'home_img_4.jpg', content_type: 'image/jpg')
