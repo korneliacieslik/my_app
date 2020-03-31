bca3c7fcf211fcb2df3a0f358168297bd11cc106
Kornelia <korneliacieslik08@gmail.com>
2020-03-31 19:52:56 +0200

    Comment email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,19 +111,19 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'https://healthybowl.herokuapp.com', port: 80 }
 
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV["SENDMAIL_USERNAME"],
-    password:       ENV["SENDMAIL_PASSWORD"],
-    domain:        "gmail.com",
-    address:       "smtp.gmail.com",
-    port:          "587",
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      ENV["SENDMAIL_USERNAME"],
+  #   password:       ENV["SENDMAIL_PASSWORD"],
+  #   domain:        "gmail.com",
+  #   address:       "smtp.gmail.com",
+  #   port:          "587",
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_caching = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
 
 end
