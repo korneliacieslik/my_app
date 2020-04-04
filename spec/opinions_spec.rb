2e755cddee5fa08dbcdd078b9e988170ed11a09d
Kornelia <korneliacieslik08@gmail.com>
2020-04-04 17:06:03 +0200

    Add option to watch test runing in the browser.

--- spec/opinions_spec.rb
+++ spec/opinions_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.describe 'You about us', type: :feature do
   scenario 'index page' do
     visit opinions_path 
+    sleep(5)
     expect(page).to have_content('You about us')
   end
 end
