cb5af7c9ef636c79f0cf2bb1c445c3103786ea04
Kornelia <korneliacieslik08@gmail.com>
2020-04-21 23:27:41 +0200

    Deal with nil session[:cart]

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   resources :carts
   devise_for :users, controllers: { registrations: 'users/registrations',  omniauth_callbacks: 'users/omniauth_callbacks'  }
   
-  # devise_scope :user do
-  #  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
-  
-  resources  :opinions, :orders, :products
+  resources  :opinions, :orders, :products 
 
   root 'static_pages#home'
   
