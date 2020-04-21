c171bea54bcc512ef3409946560eefa6783a98ff
Kornelia <korneliacieslik08@gmail.com>
2020-04-21 23:39:14 +0200

    Clean up

--- test/controllers/orders_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# Require test_helper.rb file to load configuration to run tests. 
-# If it is included to file with tests, method included in this file are available
-# for these tests.
-
-require 'test_helper'
-
-class OrdersControllerTest < ActionDispatch::IntegrationTest
-  
-end
