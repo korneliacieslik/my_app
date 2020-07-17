e02bf2243beed8741de58be4e3fde12973ce365b
Kornelia <korneliacieslik08@gmail.com>
2020-07-17 11:44:05 +0200

    Fix Syntaxerror

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,4 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'https://healthybowl.herokuapp.com', port: 80 }
 
- 
+ end 
